3daf4bd3875aa8956465cf3301b7c1f0333a9f53
CMSdossantos <cmsdossantos@gmail.com>
2022-08-18 18:18:32 +0200

    npm react-chartjs-2 installed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import React from 'react';
-import { VictoryBar, VictoryChart } from "victory";
+
 
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <VictoryChart>
-        <VictoryBar/>
-
-      </VictoryChart>
+      
     </div>
     
   );
